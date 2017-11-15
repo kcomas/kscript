11a7382c712d2e8a849c0821701edd5e3a3e0f37
kcomas <kevin.comas.git@gmail.com>
2017-11-14 19:48:33 -0500

    fix match

--- src/lang/parser/token_container.rs
+++ src/lang/parser/token_container.rs
@@ -119,11 +119,26 @@ impl TokenContainer {
         self.current_slice.position += 1;
     }
 
-    pub fn get_register_and_use(&mut self, pos: usize) -> Option<usize> {
-        if let Some(token) = self.get_mut(pos) {
-            if let Some(reg_counter) = token.is_register() {
-                *token = Token::Used;
-                return Some(reg_counter);
+    pub fn get_register_and_use(&mut self, mut pos: usize, inc_up: bool) -> Option<usize> {
+        let start = self.current_slice.start;
+        let end = self.current_slice.end;
+        loop {
+            if let Some(token) = self.get_mut(pos) {
+                if let Some(reg_counter) = token.is_register() {
+                    *token = Token::Used;
+                    return Some(reg_counter);
+                } else {
+                    match *token {
+                        Token::Used => {
+                            if inc_up && pos < end {
+                                pos += 1;
+                            } else if pos > start {
+                                pos -= 1;
+                            }
+                        }
+                        _ => break,
+                    };
+                }
             }
         }
         None
@@ -132,12 +147,12 @@ impl TokenContainer {
     pub fn get_right_register_and_use(&mut self) -> Option<usize> {
         // get the right value if register and set to used
         let pos = self.current_slice.position + 1;
-        self.get_register_and_use(pos)
+        self.get_register_and_use(pos, true)
     }
 
     pub fn get_left_register_and_use(&mut self) -> Option<usize> {
         let pos = self.current_slice.position - 1;
-        self.get_register_and_use(pos)
+        self.get_register_and_use(pos, false)
     }
 
     pub fn get_slice_token_index(&self) -> usize {
