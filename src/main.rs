8bb6dd19b29ffd77a14a3e58e8c9730c2cb0cc16
kcomas <kevin.comas.git@gmail.com>
2017-11-14 21:37:10 -0500

    don't panic on error

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ use kscript::lang::logger::{Logger, DebugLogger, LoggerMode};
 fn main() {
     let mut kscript = Kscript::new(DebugLogger::new(LoggerMode::Stdout));
     if let Err(kerror) = kscript.run("a = !@[\"ls\", \"-lh\"]; a[1] > 1") {
-        panic!("{:?}", kerror);
+        eprintln!("Error {:?}", kerror);
     }
 }
