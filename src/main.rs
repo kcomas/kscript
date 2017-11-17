13de780202ca11a4f9e68d06f757b2ffa40f9006
kcomas <kevin.comas.git@gmail.com>
2017-11-17 00:58:54 -0500

    add more complex equation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use kscript::lang::logger::{Logger, DebugLogger, LoggerMode};
 
 fn main() {
     let mut kscript = Kscript::new(DebugLogger::new(LoggerMode::Stdout));
-    if let Err(kerror) = kscript.run("a = (1 + 2 * 3 ^ 5)") {
+    if let Err(kerror) = kscript.run("a = (1.234 * ((2 + 4.3) % 2) + 1 ^ 5)") {
         eprintln!("Error {:?}", kerror);
     }
 }
