de0dfda3269e9cb119a2b994d9f36d6027d6f2ff
kcomas <kevin.comas.git@gmail.com>
2017-11-24 15:14:39 -0500

    add test for math

--- tests/vm.rs
+++ tests/vm.rs
@@ -24,3 +24,16 @@ fn constant_assign_float() {
     let v = kscript.get_root_scope().get_const("TEST").unwrap();
     assert_eq!(*v.borrow(), DataHolder::Anon(DataType::Float(1234.123)));
 }
+
+#[test]
+fn var_assign_math() {
+    let kscript = create(
+        "a = (1.234 * ((2 + 4.3) % 2) + 1 ^ 5)",
+        VoidLogger::new(LoggerMode::Void),
+    );
+    let v = kscript.get_root_scope().get_var("a").unwrap();
+    assert_eq!(
+        *v.borrow(),
+        DataHolder::Anon(DataType::Float(1.3701999999999996))
+    );
+}
