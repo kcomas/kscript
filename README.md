d0bbc7db3f6a13cab4cf16e11a8ab9b0eca138e2
kcomas <kevin.comas.git@gmail.com>
2017-11-27 20:16:53 -0500

    add title to status

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 Work in progress see [Overview](design/OVERVIEW.md)
 
-[Trello](https://trello.com/b/IevrZUGw/kscript)
+### Current Status
+* [Trello](https://trello.com/b/IevrZUGw/kscript)
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ use kscript::lang::logger::{Logger, DebugLogger, LoggerMode};
 
 fn main() {
     let mut kscript = Kscript::new(DebugLogger::new(LoggerMode::Stdout));
-    if let Err(kerror) = kscript.run(
-        "a = @[2, 5]; b = {|| 4}; c = %[\"t\": 2, \"g\": 1]; d = (a[1] + b|| + c[\"g\"]); d >> 1",
-    )
-    {
+    if let Err(kerror) = kscript.run("a = {|a| a}; (a|2| + 3) >> 1") {
         eprintln!("Error {:?}", kerror);
     }
 }
