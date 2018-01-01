394195712f2ff896e9cdc0436ebaf9b6e5bb1dc3
kcomas <kevin.comas.git@gmail.com>
2018-01-01 16:41:10 -0500

    split up ast

---
+++ src/ast/ast.rs
@@ -0,0 +1,224 @@
+use super::super::data_type::DataType;
+use super::super::error::Error;
+
+#[derive(Debug, Clone)]
+pub enum Ast {
+    Used,
+    Comment(String),
+    End,
+    Var(String),
+    Bool(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+    Group(Vec<Ast>), // (...)
+    // var, args, body
+    Function(Box<Ast>, Vec<Vec<Ast>>, Vec<Ast>),
+    If(Vec<Ast>),
+    Assign,
+    Equals,
+    Add,
+    Sub,
+    Mul,
+    Exp,
+    Div,
+    Rem,
+    IoWrite,
+    IoAppend,
+    Return,
+}
+
+impl<'a> Ast {
+    pub fn presedence(&self) -> usize {
+        if self.is_data() {
+            return 1;
+        }
+        match *self {
+            Ast::Return => 2,
+            Ast::IoWrite | Ast::IoAppend | Ast::Assign => 3,
+            Ast::If(_) => 4,
+            Ast::Equals => 5,
+            Ast::Add | Ast::Sub => 6,
+            Ast::Mul | Ast::Div | Ast::Rem => 7,
+            Ast::Exp => 8,
+            Ast::Function(_, _, _) => 9,
+            _ => 0,
+        }
+    }
+
+    pub fn is_end(&self) -> bool {
+        if let Ast::End = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn is_used(&self) -> bool {
+        if let Ast::Used = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn is_bool(&self) -> bool {
+        if let Ast::Bool(_) = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn is_number(&self) -> bool {
+        match *self {
+            Ast::Integer(_) | Ast::Float(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_string(&self) -> bool {
+        if let Ast::String(_) = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn is_data(&self) -> bool {
+        self.is_var() || self.is_number() || self.is_string() || self.is_bool()
+    }
+
+    pub fn is_var(&self) -> bool {
+        if let Ast::Var(_) = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn get_var_name(&self) -> Result<&str, Error<'a>> {
+        match *self {
+            Ast::Var(ref name) => Ok(name),
+            _ => Err(Error::AstNotVar("Token not a var")),
+        }
+    }
+
+    pub fn is_group(&self) -> bool {
+        if let Ast::Group(_) = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn get_group_body_mut(&mut self) -> Result<&mut Vec<Ast>, Error<'a>> {
+        if let Ast::Group(ref mut body) = *self {
+            return Ok(body);
+        }
+        Err(Error::InvalidGroup("Not a group"))
+    }
+
+    pub fn is_function(&self) -> bool {
+        match *self {
+            Ast::Function(_, _, _) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_function_def(&self) -> bool {
+        match *self {
+            Ast::Function(_, _, ref body) => body.len() > 0,
+            _ => false,
+        }
+    }
+
+    pub fn get_function_name(&self) -> Result<&str, Error<'a>> {
+        match *self {
+            Ast::Function(ref name_token, _, _) => match **name_token {
+                Ast::Var(ref name) => Ok(name),
+                _ => Err(Error::AstIsNotAFunction("Function name is not a var")),
+            },
+            _ => Err(Error::AstIsNotAFunction("Not a function cannot get name")),
+        }
+    }
+
+    pub fn get_function_args(&self) -> Result<&Vec<Vec<Ast>>, Error<'a>> {
+        match *self {
+            Ast::Function(_, ref args, _) => Ok(args),
+            _ => Err(Error::AstIsNotAFunction(
+                "Not a function cannot get function args",
+            )),
+        }
+    }
+
+    pub fn get_function_args_mut(&mut self) -> Result<&mut Vec<Vec<Ast>>, Error<'a>> {
+        match *self {
+            Ast::Function(_, ref mut args, _) => Ok(args),
+            _ => Err(Error::AstIsNotAFunction(
+                "Not a function cannot get function args mut",
+            )),
+        }
+    }
+
+    pub fn get_function_body_mut(&mut self) -> Result<&mut Vec<Ast>, Error<'a>> {
+        match *self {
+            Ast::Function(_, _, ref mut body) => Ok(body),
+            _ => Err(Error::AstIsNotAFunction(
+                "Not a function cannot get function body",
+            )),
+        }
+    }
+
+    // have to specify
+    pub fn is_dyadic(&self) -> bool {
+        match *self {
+            Ast::Assign
+            | Ast::Equals
+            | Ast::Add
+            | Ast::Sub
+            | Ast::Mul
+            | Ast::Exp
+            | Ast::Div
+            | Ast::Rem
+            | Ast::IoWrite
+            | Ast::IoAppend => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_monadic_left(&self) -> bool {
+        match *self {
+            Ast::Return => true,
+            _ => false,
+        }
+    }
+
+    pub fn to_data_type(&self) -> Result<DataType, Error<'a>> {
+        match *self {
+            Ast::Bool(b) => Ok(DataType::Bool(b)),
+            Ast::Integer(int) => Ok(DataType::Integer(int)),
+            Ast::Float(float) => Ok(DataType::Float(float)),
+            Ast::String(ref string) => Ok(DataType::String(string.clone())),
+            _ => Err(Error::CannotConvertToDataType(
+                self.clone(),
+                "Cannot convert to data type",
+            )),
+        }
+    }
+
+    pub fn is_if(&self) -> bool {
+        if let Ast::If(_) = *self {
+            return true;
+        }
+        false
+    }
+
+    pub fn get_if_body_mut(&mut self) -> Result<&mut Vec<Ast>, Error<'a>> {
+        if let Ast::If(ref mut body) = *self {
+            return Ok(body);
+        }
+        Err(Error::CannotGetIfBody("Not an if"))
+    }
+
+    pub fn is_assign(&self) -> bool {
+        if let Ast::Assign = *self {
+            return true;
+        }
+        false
+    }
+}
--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,229 +1,9 @@
+mod ast;
+
 use std::str::Chars;
 use std::iter::Peekable;
-use super::data_type::DataType;
 use super::error::Error;
-
-#[derive(Debug, Clone)]
-pub enum Ast {
-    Used,
-    Comment(String),
-    End,
-    Var(String),
-    Bool(bool),
-    Integer(i64),
-    Float(f64),
-    String(String),
-    Group(Vec<Ast>), // (...)
-    // var, args, body
-    Function(Box<Ast>, Vec<Vec<Ast>>, Vec<Ast>),
-    If(Vec<Ast>),
-    Assign,
-    Equals,
-    Add,
-    Sub,
-    Mul,
-    Exp,
-    Div,
-    Rem,
-    IoWrite,
-    IoAppend,
-    Return,
-}
-
-impl<'a> Ast {
-    pub fn presedence(&self) -> usize {
-        if self.is_data() {
-            return 1;
-        }
-        match *self {
-            Ast::Return => 2,
-            Ast::IoWrite | Ast::IoAppend | Ast::Assign => 3,
-            Ast::If(_) => 4,
-            Ast::Equals => 5,
-            Ast::Add | Ast::Sub => 6,
-            Ast::Mul | Ast::Div | Ast::Rem => 7,
-            Ast::Exp => 8,
-            Ast::Function(_, _, _) => 9,
-            _ => 0,
-        }
-    }
-
-    pub fn is_end(&self) -> bool {
-        if let Ast::End = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn is_used(&self) -> bool {
-        if let Ast::Used = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn is_bool(&self) -> bool {
-        if let Ast::Bool(_) = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn is_number(&self) -> bool {
-        match *self {
-            Ast::Integer(_) | Ast::Float(_) => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_string(&self) -> bool {
-        if let Ast::String(_) = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn is_data(&self) -> bool {
-        self.is_var() || self.is_number() || self.is_string() || self.is_bool()
-    }
-
-    pub fn is_var(&self) -> bool {
-        if let Ast::Var(_) = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn get_var_name(&self) -> Result<&str, Error<'a>> {
-        match *self {
-            Ast::Var(ref name) => Ok(name),
-            _ => Err(Error::AstNotVar("Token not a var")),
-        }
-    }
-
-    pub fn is_group(&self) -> bool {
-        if let Ast::Group(_) = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn get_group_body_mut(&mut self) -> Result<&mut Vec<Ast>, Error<'a>> {
-        if let Ast::Group(ref mut body) = *self {
-            return Ok(body);
-        }
-        Err(Error::InvalidGroup("Not a group"))
-    }
-
-    pub fn is_function(&self) -> bool {
-        match *self {
-            Ast::Function(_, _, _) => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_function_def(&self) -> bool {
-        match *self {
-            Ast::Function(_, _, ref body) => body.len() > 0,
-            _ => false,
-        }
-    }
-
-    pub fn get_function_name(&self) -> Result<&str, Error<'a>> {
-        match *self {
-            Ast::Function(ref name_token, _, _) => match **name_token {
-                Ast::Var(ref name) => Ok(name),
-                _ => Err(Error::AstIsNotAFunction("Function name is not a var")),
-            },
-            _ => Err(Error::AstIsNotAFunction("Not a function cannot get name")),
-        }
-    }
-
-    pub fn get_function_args(&self) -> Result<&Vec<Vec<Ast>>, Error<'a>> {
-        match *self {
-            Ast::Function(_, ref args, _) => Ok(args),
-            _ => Err(Error::AstIsNotAFunction(
-                "Not a function cannot get function args",
-            )),
-        }
-    }
-
-    pub fn get_function_args_mut(&mut self) -> Result<&mut Vec<Vec<Ast>>, Error<'a>> {
-        match *self {
-            Ast::Function(_, ref mut args, _) => Ok(args),
-            _ => Err(Error::AstIsNotAFunction(
-                "Not a function cannot get function args mut",
-            )),
-        }
-    }
-
-    pub fn get_function_body_mut(&mut self) -> Result<&mut Vec<Ast>, Error<'a>> {
-        match *self {
-            Ast::Function(_, _, ref mut body) => Ok(body),
-            _ => Err(Error::AstIsNotAFunction(
-                "Not a function cannot get function body",
-            )),
-        }
-    }
-
-    // have to specify
-    pub fn is_dyadic(&self) -> bool {
-        match *self {
-            Ast::Assign
-            | Ast::Equals
-            | Ast::Add
-            | Ast::Sub
-            | Ast::Mul
-            | Ast::Exp
-            | Ast::Div
-            | Ast::Rem
-            | Ast::IoWrite
-            | Ast::IoAppend => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_monadic_left(&self) -> bool {
-        match *self {
-            Ast::Return => true,
-            _ => false,
-        }
-    }
-
-    pub fn to_data_type(&self) -> Result<DataType, Error<'a>> {
-        match *self {
-            Ast::Bool(b) => Ok(DataType::Bool(b)),
-            Ast::Integer(int) => Ok(DataType::Integer(int)),
-            Ast::Float(float) => Ok(DataType::Float(float)),
-            Ast::String(ref string) => Ok(DataType::String(string.clone())),
-            _ => Err(Error::CannotConvertToDataType(
-                self.clone(),
-                "Cannot convert to data type",
-            )),
-        }
-    }
-
-    pub fn is_if(&self) -> bool {
-        if let Ast::If(_) = *self {
-            return true;
-        }
-        false
-    }
-
-    pub fn get_if_body_mut(&mut self) -> Result<&mut Vec<Ast>, Error<'a>> {
-        if let Ast::If(ref mut body) = *self {
-            return Ok(body);
-        }
-        Err(Error::CannotGetIfBody("Not an if"))
-    }
-
-    pub fn is_assign(&self) -> bool {
-        if let Ast::Assign = *self {
-            return true;
-        }
-        false
-    }
-}
+pub use self::ast::Ast;
 
 pub fn load_ast<'a>(iter: &mut Peekable<Chars>) -> Result<Vec<Ast>, Error<'a>> {
     let mut ast = Vec::new();
@@ -261,6 +41,16 @@ fn load_statement<'a>(iter: &mut Peekable<Chars>) -> Result<Option<Ast>, Error<'
             '.' => {
                 iter.next();
                 let fn_name = load_var(iter)?;
+                // find first ,
+                loop {
+                    let c = match iter.next() {
+                        Some(c) => c,
+                        None => return Err(Error::InvalidArgs("No more charaters")),
+                    };
+                    if c == ',' {
+                        break;
+                    }
+                }
                 let fn_args = load_args(iter)?;
                 let body = load_block(iter, '{', '}')?;
                 return Ok(Some(Ast::Function(Box::new(fn_name), fn_args, body)));
@@ -361,16 +151,6 @@ fn load_string<'a>(iter: &mut Peekable<Chars>) -> Result<Ast, Error<'a>> {
 }
 
 fn load_args<'a>(iter: &mut Peekable<Chars>) -> Result<Vec<Vec<Ast>>, Error<'a>> {
-    // find first ,
-    loop {
-        let c = match iter.next() {
-            Some(c) => c,
-            None => return Err(Error::InvalidArgs("No more charaters")),
-        };
-        if c == ',' {
-            break;
-        }
-    }
     let mut args = Vec::new();
     let mut current_arg = Vec::new();
     loop {
